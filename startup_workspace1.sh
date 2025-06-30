fe5b068a4eaeec487eb7e789eab38be2fe6bafad
ArefOleg <serd-075@mail.ru>
2025-06-30 11:26:04 +0000

    Update startup_workspace1.sh

--- startup_workspace1.sh
+++ startup_workspace1.sh
@@ -5,54 +5,53 @@ LAYOUT_FILE="/tmp/i3_layout_$(date +%s).json"
 
 cat > "$LAYOUT_FILE" << 'EOF'
 {
-    "layout": "splith",
     "type": "con",
-    "percent": 100,
-    "nodes": [
+    "layout": "splith",
+    "nodes":[
         {
             "type": "con",
-            "percent": 50,
+            "percent": 0.4,
             "layout": "splitv",
-            "nodes": [
-                {
-                    "type": "con",
-                    "percent": 50,
-                    "swallows": [{"class": "DUF"}]
-                },
-                {
-                    "type": "con",
-                    "percent": 50,
-                    "swallows": [{"class": "CONSOLE"}]
-                }
+            "nodes":[
+                {"type": "con",
+                "percent": 0.5,
+                "swallows":[{"class":"DUF"}]},
+                
+                {"type": "con",
+                "percent": 0.25,
+                "swallows":[{"class":"TASK"}]},
+
+                {"type": "con",
+                "percent": 0.25,
+                "swallows":[{"class":"CONSOLE"}]}
+
+                
             ]
         },
         {
             "type": "con",
-            "percent": 50,
+            "percent": 0.6,
             "layout": "splitv",
-            "nodes": [
-                {
-                    "type": "con",
-                    "percent": 60,
-                    "swallows": [{"class": "BTOP"}]
-                },
-                {
-                    "type": "con",
-                    "layout": "splith",
-                    "percent": 40,
-                    "nodes": [
-                        {
-                            "type": "con",
-                            "percent": 50,
-                            "swallows": [{"class": "WEATHER"}]
-                        },
-                        {
-                            "type": "con",
-                            "percent": 50,
-                            "swallows": [{"class": "RSS"}]
-                        }
-                    ]
+            "nodes":[
+                {"type": "con",
+                "percent": 0.6,
+                "swallows":[{"class":"BTOP"}]},
+
+                {"type": "con",
+                "percent": 0.4,
+                "layout": "splith",
+                "nodes":[
+                    {"type": "con",
+                    "percent": 0.5,
+                    "swallows":[{"class":"WEATHER"}]},
+
+                    {"type": "con",
+                    "percent": 0.5,
+                    "swallows":[{"class":"RSS"}]}
+                ]
                 }
+                
+                
             ]
         }
     ]
@@ -67,11 +66,13 @@ nohup alacritty --class 'DUF' -e bash -c "while true; do clear; duf; sleep 5; do
 nohup alacritty --class 'CONSOLE' >/dev/null 2>&1 &
 nohup alacritty --class 'BTOP' -e btop >/dev/null 2>&1 &
 nohup alacritty --class 'WEATHER' -e python ~/user_programs/weather/weather.py >/dev/null 2>&1 &
+nohup alacritty --class 'TASK' -e python ~/user_programs/task_manager/task_manager.py >/dev/null 2>&1 &
 nohup alacritty --class 'RSS' -e python ~/user_programs/rss/get_rss.py >/dev/null 2>&1 &
 
+
 # Удаляем временный файл
-sleep 1
-rm "$LAYOUT_FILE"
+#sleep 1
+#rm "$LAYOUT_FILE"
 
 # Сообщение для проверки
-echo "Layout applied. Check your i3 workspace."
+#echo "Layout applied. Check your i3 workspace."
